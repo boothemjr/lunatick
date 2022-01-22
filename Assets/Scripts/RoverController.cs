6cf5f19023a74bb6e556b199cd5c9fdea8d625b7
Maurice Boothe Jr <maurice.boothe.jr@gmail.com>
2022-01-22 12:48:42 -0500

    add WASD control

--- Assets/Scripts/RoverController.cs
+++ Assets/Scripts/RoverController.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class RoverController : MonoBehaviour
 {
+
+    public WheelJoint2D wheelFront;
+    public WheelJoint2D wheelMid;
+    public WheelJoint2D wheelRear;
     
+    public JointMotor2D motorFront;
+    public JointMotor2D motorMid;
+    public JointMotor2D motorRear;
+
+    public float speedForward;
+    public float speedBackward;
+    
+    public float torqueForward;
+    public float torqueBackward;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +28,43 @@ public class RoverController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxisRaw("Vertical") > 0) // using inputManager for vehicle control - might change later
+        {
+            motorFront.motorSpeed = speedForward * -1;
+            motorFront.maxMotorTorque = torqueForward;
+            wheelFront.motor = motorFront;
+            
+            motorMid.motorSpeed = speedForward * -1;
+            motorMid.maxMotorTorque = torqueForward;
+            wheelMid.motor = motorMid;
+            
+            motorRear.motorSpeed = speedForward * -1;
+            motorRear.maxMotorTorque = torqueForward;
+            wheelRear.motor = motorRear;
+
+        }
         
+        else if (Input.GetAxisRaw("Vertical") < 0) 
+        {
+            motorFront.motorSpeed = speedBackward * -1;
+            motorFront.maxMotorTorque = torqueBackward;
+            wheelFront.motor = motorFront;
+            
+            motorMid.motorSpeed = speedBackward * -1;
+            motorMid.maxMotorTorque = torqueBackward;
+            wheelMid.motor = motorMid;
+            
+            motorRear.motorSpeed = speedBackward * -1;
+            motorRear.maxMotorTorque = torqueBackward;
+            wheelRear.motor = motorRear;
+
+        }
+        else
+        {
+            wheelFront.useMotor = false;
+            wheelMid.useMotor = false;
+            wheelRear.useMotor = false;
+            
+        }
     }
 }
